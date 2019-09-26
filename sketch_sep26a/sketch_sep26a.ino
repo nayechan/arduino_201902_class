1c8ef4e7b237fd15580f588658d40e772e090922
nayechan <66f@naver.com>
2019-09-26 13:46:39 +0900

    Create sketch_sep26a.ino

---
+++ sketch_sep26a/sketch_sep26a.ino
@@ -0,0 +1,18 @@
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello World.");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  char read_data;
+  if(Serial.available())
+  {
+    read_data = Serial.read();
+    Serial.print(read_data);
+  }
+  delay(10);
+}
